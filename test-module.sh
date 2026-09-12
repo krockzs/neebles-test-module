882cab1756e1dd10af4aae0d703a7299f214aaef
Olate Stark y Jarvis <47009066+krockzs@users.noreply.github.com>
2026-09-12 05:26:31 -0300

    Add test module GUI for Boss lifecycle testing

--- test-module.sh
+++ test-module.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+set -e
+
+MODULE_DIR="$(
+    cd "$(dirname "$0")"
+    pwd
+)"
+
 case "${1:-default}" in
     open)
-        echo "N.E.E.B.L.E.S. Test Module 1.0.0 - OPEN"
+        exec python3 \
+            "$MODULE_DIR/test-module-gui.py"
         ;;
     *)
         echo "N.E.E.B.L.E.S. Test Module 1.0.0"
